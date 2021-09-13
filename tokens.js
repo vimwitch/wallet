faac5f2785a3f2bffb0d7a9ea4e5e1057e5e2647
Chance Hudson <jchancehud@gmail.com>
2021-09-13 11:06:16 -0500

    add usdc

--- tokens.js
+++ tokens.js
@@ -226,6 +226,7 @@ module.exports = {
     dai: '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
     weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
     link: '0x514910771af9ca656af840dff83e8264ecf986ca', // link
+    usdc: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // usdc
   },
   ERC20ABI,
 }
